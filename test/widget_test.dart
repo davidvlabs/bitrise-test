82fbafc5f62542e215e59106f864147168ac36e8
pappda <papp.david@vlabs.at>
2020-04-16 19:50:02 +0200

    fix tests: add dotenv

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bitrise_test/main.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
+  setUp(() async {
+    await DotEnv().load('dotenv.env');
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
